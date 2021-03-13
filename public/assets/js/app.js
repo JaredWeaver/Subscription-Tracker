ed815ec330f312018b9fdc073ab797bfec1b0fdc
JaredWeav <jared.weaver2@gmail.com>
2021-03-13 12:07:48 -0500

    refactored js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,130 +1,204 @@
-$('#add-user').on('click', function (event) {
-  event.preventDefault();
+const addUserBtn = document.getElementById('add-user');
+const createErrMsg = document.getElementById('create-err-msg');
+const updateUserBtn = document.getElementById('update-user');
+const changeUserModal = document.getElementById('change-user-modal')
+const errMsg = document.getElementById('err-msg');
+const updateErrMsg = document.getElementById('update-err-msg');
+const deleteUserBtn = document.getElementById('delete-user');
+const deleteUserModal = document.getElementById('delete-user-modal');
+const confirmDeleteBtn = document.getElementById('confirm-delete');
+const registerBtn = document.getElementById('register');
+const loginModal = document.getElementById('login-modal');
+const goHomeBtn = document.getElementById('go-home');
+const loginBtn = document.getElementById('login');
+const userInfoModal = document.getElementById('user-info');
+const loginErrMsg = document.getElementById('login-err-msg');
+
+console.log(addUserBtn);
+
+addUserBtn.addEventListener('click', (e) => {
+  console.log(e);
+  e.preventDefault();
 
   const newAccount = {
-    firstName: $('#inputFirst').val().trim(),
-    lastName: $('#inputLast').val().trim(),
-    email: $('#inputEmail').val().trim(),
-    password: $('#inputPassword').val().trim()
+    firstName: document.getElementById('inputFirst').value.trim(),
+    lastName: document.getElementById('inputLast').value.trim(),
+    email: document.getElementById('inputEmail').value.trim(),
+    password: document.getElementById('inputPassword').value.trim(),
   };
 
-  if (newAccount.password.length > 0 && newAccount.email.length > 0 && newAccount.password.length > 0 && newAccount.lastName.length > 0 && newAccount.firstName.length > 0) {
-    $.ajax({
-      type: 'POST',
-      url: '/api/register',
-      data: newAccount
-    }).then(() => {
+  if (
+    newAccount.password.length > 0 &&
+    newAccount.email.length > 0 &&
+    newAccount.password.length > 0 &&
+    newAccount.lastName.length > 0 &&
+    newAccount.firstName.length > 0
+  ) {
+    fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newAccount),
+    }).then((response) => {
       window.location.href = '/';
     });
   } else {
     console.log('**Please fill out entire form**');
-    $('#create-err-msg').empty('').text('**Please fill out entire form**');
+    createErrMsg.textContent = '**Please fill out entire form**';
   }
 });
 
-$('#update-user').on('click', function (event) {
-  event.preventDefault();
+updateUserBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  //   const id = $(this).data('id');
+  const id = e.target.getAttribute('data-id'); //Jared - need to add foreach??
 
-  const id = $(this).data('id');
-
-  // capture All changes
   const changeUser = {
-    firstName: $('#inputFirst').val().trim(),
-    lastName: $('#inputLast').val().trim(),
-    email: $('#inputEmail').val().trim(),
-    password: $('#inputPassword').val().trim()
+    firstName: document.getElementById('inputFirst').value.trim(),
+    lastName: document.getElementById('inputLast').value.trim(),
+    email: document.getElementById('inputEmail').value.trim(),
+    password: document.getElementById('inputPassword').value.trim(),
   };
-  $('#err-msg').empty('');
+
+  errMsg.value = '';
+  changeUserModal.style.display = 'block';
   // $('#change-user-modal').modal('show');
   console.log(changeUser);
 
-  if (changeUser.password.length > 0 && changeUser.email.length > 0 && changeUser.password.length > 0 && changeUser.lastName.length > 0 && changeUser.firstName.length > 0) {
-    $.ajax({
-      type: 'PUT',
-      url: `/api/user/${id}`,
-      data: changeUser
+  if (
+    changeUser.password.length > 0 &&
+    changeUser.email.length > 0 &&
+    changeUser.password.length > 0 &&
+    changeUser.lastName.length > 0 &&
+    changeUser.firstName.length > 0
+  ) {
+    fetch(`/api/user/${id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify(changeUser),
     }).then((result) => {
       console.log('Updated user:', result);
-      // Reload the page to get the updated list
       window.location.href = '/logout';
     });
   } else {
     console.log('**Please fill out entire form**');
-    $('#update-err-msg').empty('').text('**Please fill out entire form**');
+    updateErrMsg.textContent = '**Please fill out entire form**';
+    //JARED --> not sure if this is proper syntax
   }
 });
 
-// DELETE   ***************************************************
-$('#delete-user').on('click', function (event) {
-  event.preventDefault();
-  $('#err-msg').empty('');
-  $('#delete-user-modal').modal('show');
+
+deleteUserBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  errMsg.value = '';
+
+  deleteUserModal.style.display = 'block';
 });
 
-$('#confirm-delete').on('click', function (event) {
-  event.preventDefault();
 
-  const id = $(this).data('id');
+confirmDeleteBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const id = e.target.getAttribute('data-id'); //Jared - need to add foreach??
 
   const deleteUser = {
-    email: $('#userEmail').val().trim(),
-    password: $('#userPassword').val().trim()
+
+    email: document.getElementById('userEmail').value.trim(),
+    password: document.getElementById('userPassword').value.trim(),
   };
 
   if (deleteUser.email.length > 0 && deleteUser.password.length > 0) {
-    $.ajax({
-      type: 'POST',
-      url: '/api/user/confirm',
-      data: deleteUser
+    fetch(`/api/user/confirm`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(deleteUser),
     }).then((result) => {
       if (result) {
-        $.ajax(`/api/user/${id}`, {
-          type: 'DELETE'
+        fetch(`/api/user/${id}`, {
+          method: 'PUT',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(deleteUser),
         }).then(() => {
-          console.log('Deleted user', deleteUser);
-          // Reload the page to get the updated list
+          console.log('deleted user:', deleteUser);
           window.location.href = '/logout';
         });
       } else {
-        $('#err-msg').empty('').text('Wrong credentials!');
+        errMsg.textContent = 'Wrong Credentials!';
       }
     });
   } else {
-    console.log('fill out entire form');
-    $('#err-msg').empty('').text('fill out entire form');
+    //     console.log('fill out entire form');
+    //     $('#err-msg').empty('').text('fill out entire form');
+    console.log('fill out the entire form');
+    errMsg.textContent = 'Fill out the entire form!';
   }
 });
 
-$('#register').on('click', function (event) {
-  event.preventDefault();
+// $('#register').on('click', function (event) {
+registerBtn.addEventListener('click', (e) => {
+  e.preventDefault();
   window.location.href = '/register';
 });
 
-$('#login-modal').on('click', function (event) {
-  event.preventDefault();
-  $('#user-info').modal('show');
+// $('#login-modal').on('click', function (event) {
+loginModal.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  userInfoModal.style.display = 'block';
+
+  // $('#user-info').modal('show');
 });
 
-$('#go-home').on('click', function (event) {
-  event.preventDefault();
+// $('#go-home').on('click', function (event) {
+goHomeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
   window.location.href = '/';
 });
 
-$('#login').on('click', function (event) {
-  event.preventDefault();
+// $('#login').on('click', function (event) {
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
 
-  const user = {
-    email: $('#email').val().trim(),
-    password: $('#user_password').val().trim()
+  const newAccount = {
+    email: document.getElementById('email').value.trim(),
+    password: document.getElementById('user_password').value.trim(),
+    // email: $('#email').val().trim(),
+    // password: $('#user_password').val().trim(),
   };
 
-  $.post('/api/login', user, (result) => {
-    // console.log(result);
-    if (result.loggedIn) {
-      $(document.location).attr('href', '/dashboard');
+  fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newAccount),
+  }).then((response) => {
+    if (response.loggedIn) {
+      window.location.href = '/dashboard'; //Jared - might not be correct syntax here
     } else {
-      $('#login-err-msg').empty('').text(result.error);
-      $('#user-info').modal('hide');
+      loginErrMsg.value = '';
+      userInfoModal.style.display = 'none';
     }
   });
+  //   $.post('/api/login', user, (result) => {
+  //     // console.log(result);
+  //     if (result.loggedIn) {
+  //       $(document.location).attr('href', '/dashboard');
+  //     } else {
+  //       $('#login-err-msg').empty('').text(result.error);
+  //       $('#user-info').modal('hide');
+  //     }
+  //   });
 });
